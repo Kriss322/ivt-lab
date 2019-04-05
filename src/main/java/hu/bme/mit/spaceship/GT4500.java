66aa34525bd0788fd006c19a2e93327368995070
Kristof Kassa <krist2397@gmail.com>
2019-04-05 09:18:55 +0200

    fixed FIRE ALL bug

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,6 +80,19 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
+        boolean primarySuccess = false;
+        boolean secondarySuccess = false;
+        if (! primaryTorpedoStore.isEmpty()) {
+          primarySuccess = primaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = true;
+        }
+        if (! secondaryTorpedoStore.isEmpty()) {
+          secondarySuccess = secondaryTorpedoStore.fire(1);
+          wasPrimaryFiredLast = false;
+        }
+
+        firingSuccess = primarySuccess && secondarySuccess;
+
         break;
     }
 
